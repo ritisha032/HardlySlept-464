f2ae905ad8bd3a5827452f7fc0bbe3753f816f7e
ritisha032 <ritishasingh74@gmail.com>
2023-11-21 11:08:06 +0530

    added auth route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
 import { signup,login} from "../controllers/authController.js";
+import { requireSignIn } from "../middleware/authMiddleware.js";
 const router=express.Router();
 
 router.post("/signup",signup);
 router.post("/login",login);
+
+//protected routes
+router.get("/user-auth",requireSignIn,(req,res)=>{
+    res.status(200).send({ok:true});
+});
+
 
 
 export default router;
